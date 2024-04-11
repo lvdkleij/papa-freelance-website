1ccc59f15a8633a0b214f5196888981277a13ad9
Lucas van der Kleij <kleijvander@gmail.com>
2024-04-11 14:03:36 +0200

    header setup

---
+++ src/libs/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+   selector: 'atlas-header-component',
+   standalone: true,
+   templateUrl: 'header.component.html',
+   styleUrl: 'header.component.scss',
+})
+export class AtlasHeaderComponent {}
